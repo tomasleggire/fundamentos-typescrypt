64d0ee7c0392bc4ab9841c1b20f316c096294d2f
Tomás Leggire <tomasleggire@hotmail.com>
2023-05-10 18:36:53 -0300

    Tipos inferidos

---
+++ src/03-typing.ts
@@ -0,0 +1,16 @@
+//Inferidos
+
+
+//Se agrega esa sintaxis de funcion para que sea modular el contenido
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+  const myProductName2 = 'Product 2';
+})();
