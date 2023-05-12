8b6e32a1190a0796ec99fd9fd52a89952fd30e0a
Tomás Leggire <tomasleggire@hotmail.com>
2023-05-12 18:56:51 -0300

    librerias que NO tienen soporte

---
+++ src/17-load.ts
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+
+const data = [
+  {
+    username: 'nico',
+    role: 'admin'
+  },
+  {
+    username: 'valentina',
+    role: 'seller'
+  },
+  {
+    username: 'zulema',
+    role: 'seller'
+  },
+  {
+    username: 'santiago',
+    role: 'customer'
+  }
+];
+
+
+const rta = _.groupBy(data, (item)=> item.role);
+console.log(rta);
+
+
